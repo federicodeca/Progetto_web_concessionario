ca3f99497fd17edc83b7d125485a968334ee0118
federicodeca <federicodeca98@gmail.com>
2025-05-30 18:26:02 +0200

    aggiornamento classi

--- WebApp/install/StartSmarty.php
+++ WebApp/install/StartSmarty.php
@@ -4,10 +4,10 @@ require(__DIR__ . '/../../libs/Smarty/Smarty.class.php');
 class StartSmarty{
     static function configuration(){
         $smarty=new Smarty();
-        $smarty->template_dir= __DIR__ . '/../../libs/Smarty/templates/';
-        $smarty->compile_dir= __DIR__ . '/../../libs/Smarty/templates_c/';
-        $smarty->config_dir= __DIR__ . '/../../libs/Smarty/configs/';
-        $smarty->cache_dir= __DIR__ . '/../../libs/Smarty/cache/';
+        $smarty->template_dir= __DIR__ . '/../libs/Smarty/templates/';
+        $smarty->compile_dir= __DIR__ . '/../libs/Smarty/templates_c/';
+        $smarty->config_dir= __DIR__ . '/../libs/Smarty/configs/';
+        $smarty->cache_dir= __DIR__ . '/../libs/Smarty/cache/';
         return $smarty;
     }
 }
