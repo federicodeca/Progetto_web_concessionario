39a6e5a95a48c751313c1d4a39651daea78cb8d2
Paolo <paolo.caiolfa@student.univaq.it>
2025-05-30 15:17:45 +0200

    Create FPersistentManager

---
+++ WebApp/foundation/FPersistentManager.php
@@ -0,0 +1,13 @@
+<?php
+
+class FPersistentManager {
+
+    // the variable $instance is used to store the singleton instance of the class
+    private static $instance;
+
+
+    private function __construct() {
+        // private constructor to prevent external instantiation
+    }
+
+}
