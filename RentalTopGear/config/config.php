d7902cb1eeec38cc32299f05dddb18ff97806de6
federicodeca <federicodeca98@gmail.com>
2025-07-04 00:23:24 +0200

    cambio password

---
+++ RentalTopGear/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'dealer');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'pippo');
+    define('DB_DRIVER', 'pdo_mysql');
+
+    define('COOKIE_EXP', 60 * 60 * 24 * 30); // 30 days
+
+
+    define('HOST','smtp-relay.brevo.com');
+    define('PORT', 587);
+    define('USERNAME', '90aede001@smtp-brevo.com');
+    define('PASSWORD', '');
+    
+    
